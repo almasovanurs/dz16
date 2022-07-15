1a3be447532b8054cc7133e2ef629149592caf1a
suban <nursuluu@gmx.de>
2022-07-15 22:32:49 +0200

    Initial commit

---
+++ src/com/company/Circle.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Circle {
+
+
+    public static void area(int radius){
+        double area = Math.PI * (radius * radius);
+        System.out.println(area);
+    }
+    public static void circumference(int radius){
+        double circumference = (Math.PI * 2 * radius);
+        System.out.println(circumference);
+    }
+
+
+}
+
+
+
+
+
+
